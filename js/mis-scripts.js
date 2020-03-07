0121194b734672a2b6de87150bd42cd0d9018697
Franklin Daly <dafragor@gmail.com>
2020-03-07 17:09:09 -0500

    version 1

---
+++ js/mis-scripts.js
@@ -0,0 +1,28 @@
+$(function() {
+    //==================================
+    //   1.-Configuracion RD-Navbar
+    //==================================
+    $('.rd-navbar').RDNavbar({
+        stickUpClone: false,
+        responsive: {
+            0: {
+                layout: 'rd-navbar-fixed'
+            },
+            768: {
+                layout: 'rd-navbar-static',
+                stickUp: false
+            },
+            992: {
+                stickUp: false
+            }
+
+        }
+    });
+    /* =============================================
+    2. Configuración de Universal Parallax
+    ============================================= */
+
+    new universalParallax().init({
+        speed: 6.0
+    });
+});
